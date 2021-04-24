e9d2ede4bf4137d2fa65c3eef3dd0cf3a990c5b1
raghudinkavijaykumar <raghu.dinka.vijaykumar@gmail.com>
2021-04-24 22:50:30 +0530

    file name and dir  fix

--- miner/miner/spiders/news-spider.py
+++ miner/miner/spiders/news-spider.py
@@ -1,4 +1,6 @@
+import os
 import re
+from pathlib import Path
 
 import scrapy
 from scrapy.linkextractors import LinkExtractor
@@ -11,22 +13,34 @@ class NewsSpider(scrapy.Spider):
     def start_requests(self):
         urls = [
             'https://www.moneycontrol.com/',
+            'https://economictimes.indiatimes.com/',
+            'https://www.livemint.com/',
+            'https://www.financialexpress.com/'
         ]
 
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = re.sub(r"http:|https:", "", response.url)
-        page = re.sub(r"/+", "-", page)
-        page = re.sub("^-", "", page)
-        page = re.sub("-$", "", page)
-        filename = f'data/news/{page}.html'
+        filename = f'data/{self.urlToFile(response.url)}'
+        Path(filename).parent.absolute().mkdir(parents=True, exist_ok=True)
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log(f'Saved file {filename}')
 
-        next_page = LinkExtractor()
+        next_page = LinkExtractor(deny_extensions=['html'])
         links = next_page.extract_links(response)
         for link in links:
             yield scrapy.Request(link.url, callback=self.parse)
+
+    def urlToFile(self, url):
+        f = re.sub(r"http://|https://|\?", "", url)
+        # TODO Handle this as a configuration based on file processing capability
+        f = re.sub(
+            r"\.(?!(html|htm|php|pdf|mp3|mp4|flv|mov|avi|doc|docx|xls|xlsx|md))", "-", f)
+        if f[-1] == "/":
+            return f + "index.html"
+        if "." not in f:
+            return f + ".html"
+        else:
+            return f
